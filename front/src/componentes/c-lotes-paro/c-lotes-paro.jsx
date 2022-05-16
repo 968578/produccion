41ce70b8e3d3a39cb5a697964321a340dbf41365
Omar Enrique Echavarria Ibarra <desarrollador.ti@kbk.local>
2022-05-16 07:23:14 -0500

    primer commit corrigiendo carpetas

--- front/src/componentes/c-lotes-paro/c-lotes-paro.jsx
+++ front/src/componentes/c-lotes-paro/c-lotes-paro.jsx
@@ -1,9 +1,11 @@
 import { useSelector } from "react-redux"
-import Lote from "../c-lote/c-lote"
+
 import './c-lotes-paro.css'
 
-const LotesParo=()=>{
+import Lote from "../c-lote/c-lote"
 
+
+const LotesParo = () => {
 
   const lotes = useSelector(state => state.AllLotes)
 
@@ -14,12 +16,12 @@ const LotesParo=()=>{
         <h2>Paro</h2>
       </div>
       <div className="c-lotesParo">
-      {
-        lotes && lotes.map((e,i)=>
-          e.estado ==='Paro' &&
-          <Lote key={e.op} showLote={e} index={i} />
+        {
+          lotes && lotes.map((e, i) =>
+            e.estado === 'Paro' &&
+            <Lote key={e.op} showLote={e} index={i} />
           )
-      }
+        }
       </div>
     </div>
   )
